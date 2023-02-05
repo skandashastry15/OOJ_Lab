5abc26f69f659ee7c368d3050765a49a92c6a954
Skanda M Shastry <99529191+skandashastry15@users.noreply.github.com>
2023-02-05 13:55:44 +0530

    Create thread.java

---
+++ thread.java
@@ -0,0 +1,32 @@
+class BMSCE extends Thread {
+    public void run() {
+        try {
+            for (int i = 5; i > 0; i--) {
+                System.out.println("\nBMS College Of Engineering");
+                Thread.sleep(10000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+    }
+}
+
+class CSE extends Thread {
+    public void run() {
+        try {
+            for (int i = 5; i > 0; i--) {
+                System.out.println("\nCSE");
+                Thread.sleep(2000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+    }
+}
+
+class ThreadDemo {
+    public static void main(String args[]) {
+        new BMSCE().start();
+        new CSE().start();
+    }
+}
